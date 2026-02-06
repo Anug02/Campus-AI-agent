3a007e414894134787963b670d8840088e3485ee
Anurag_Kumar <151936615+Anug02@users.noreply.github.com>
2026-02-06 15:36:14 +0530

    Add files via upload

---
+++ data_sources.py
@@ -0,0 +1,45 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9f467bf2-c2e3-4f56-8137-afc23f640675",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "EVENTS_DB = [\n",
+    "    {\"id\": 1, \"name\": \"AI Workshop\", \"date\": \"2026-02-10\", \"location\": \"Auditorium\"},\n",
+    "    {\"id\": 2, \"name\": \"Robotics Seminar\", \"date\": \"2026-02-15\", \"location\": \"Hall A\"}\n",
+    "]\n",
+    "\n",
+    "FACILITIES_DB = {\n",
+    "    \"lab_1\": {\"type\": \"Computer Lab\", \"capacity\": 40},\n",
+    "    \"room_101\": {\"type\": \"Lecture Room\", \"capacity\": 60}\n",
+    "}\n",
+    "\n",
+    "BOOKINGS_DB = []\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
